d25a5ae0ca5c06028e257945c46b7412601fe9f4
AcmenLi <676050059@qq.com>
2019-08-13 19:17:48 +0800

    8888

--- application/article/controller/v1/Nav.php
+++ application/article/controller/v1/Nav.php
@@ -25,6 +25,7 @@ class Nav
      */
     public function  getNavTempL($sign = '')
     {
+        echo 8888;die;
         $nav = NaviTempL::where('status', '=', 1)
             ->find();
         if(empty($nav)){
