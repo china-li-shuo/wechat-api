f352d456bf3b3f55db7b4707f5383cf323c5e321
AcmenLi <676050059@qq.com>
2019-08-13 19:30:50 +0800

    清除测试代码

--- application/article/controller/v1/Nav.php
+++ application/article/controller/v1/Nav.php
@@ -25,7 +25,6 @@ class Nav
      */
     public function  getNavTempL($sign = '')
     {
-        echo 'hello world123123';die;
         $nav = NaviTempL::where('status', '=', 1)
             ->find();
         if(empty($nav)){
